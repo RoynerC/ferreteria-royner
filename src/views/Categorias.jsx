a8339b0dcdfed64dac7c886eeeae1823df81d15a
KIMBERLYNG <contrerasroyner@gmail.com>
2025-10-04 19:59:30 -0600

    Agregar Boton de categorias

--- src/views/Categorias.jsx
+++ src/views/Categorias.jsx
@@ -1,5 +1,98 @@
-const Categorias = () => {
-return <h2>Página de Categoria</h2>;
-}
+import React, { useState, useEffect } from "react";
+import { Container, Row, Col, Button } from "react-bootstrap";
+import { db } from "../database/firebaseconfig";
+import { collection, getDocs, addDoc } from "firebase/firestore";
+import TablaCategorias from "../components/categorias/TablaCategoria";
+import ModalRegistroCategoria from "../components/categorias/ModalRegistroCategoria";
 
+
+const Categorias = () => {
+  const [categorias, setCategorias] = useState([]);
+  const categoriasCollection = collection(db, "categorias");
+
+      // Estados para manejo del modal de registro
+      const [mostrarModal, setMostrarModal] = useState(false);
+      const [nuevaCategoria, setNuevaCategoria] = useState({
+        nombre: "",
+        descripcion: "",
+      });
+
+      // Manejador de cambios en inputs del formulario de nueva categoría
+      const manejoCambioInput = (e) => {
+        const { name, value } = e.target;
+        setNuevaCategoria((prev) => ({
+          ...prev,
+          [name]: value,
+        }));
+      };
+
+      // Función para agregar una nueva categoría
+      const agregarCategoria = async () => {
+        // Validar campos requeridos
+        if (!nuevaCategoria.nombre || !nuevaCategoria.descripcion) {
+          alert("Por favor, completa todos los campos antes de guardar.");
+          return;
+        }
+        // Cerrar modal
+        setMostrarModal(false);
+        try {
+          // Referencia a la colección de categorías en Firestore
+          const categoriasCollection = collection(db, "categorias");
+          // Agregar a Firestore
+          await addDoc(categoriasCollection, nuevaCategoria);
+          // Limpiar campos del formulario
+          setNuevaCategoria({ nombre: "", descripcion: "" });
+          cargarCategorias();
+          console.log("Categoría agregada exitosamente.");
+        } catch (error) {
+          console.error("Error al agregar la categoría:", error);
+          alert("Error al agregar la categoría: " + error.message);
+        }
+      };
+
+  const cargarCategorias = async () => {
+    try {
+      const consulta = await getDocs(categoriasCollection);
+      const datosCategorias = consulta.docs.map((doc) => ({
+        id: doc.id,
+        ...doc.data(),
+      }));
+      setCategorias(datosCategorias);
+      console.log("Categorías cargadas desde Firestore:", datosCategorias);
+    } catch (error) {
+      console.error("Error al cargar categorías:", error);
+    }
+  };
+
+  useEffect(() => {
+    cargarCategorias();
+  });
+
+  return (
+    <Container className="mt-4">
+      <h4>Gestión de Categorías</h4>
+
+      <Row>
+        <Col lg={3} md={4} sm={4} xs={5}>
+          <Button
+            className="mb-3"
+            onClick={() => setMostrarModal(true)}
+            style={{ width: "100%" }}
+          >
+            Agregar categoría
+          </Button>
+        </Col>
+      </Row>
+      <TablaCategorias categorias={categorias} />
+
+      <ModalRegistroCategoria
+        mostrarModal={mostrarModal}
+        setMostrarModal={setMostrarModal}
+        nuevaCategoria={nuevaCategoria}
+        manejoCambioInput={manejoCambioInput}
+        agregarCategoria={agregarCategoria}
+      />
+    </Container>
+  );
+};
 export default Categorias;
